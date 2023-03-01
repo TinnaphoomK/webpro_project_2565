d3a4bc7b6378609e9abae0131c49398c99cd756c
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-03-01 17:36:24 +0700

    frontend

---
+++ space-creator/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Signin.vue')
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/Signup.vue')
+    },
+    {
+      path: '/mainpage',
+      name: 'mainpage',
+      component: () => import('../views/Mainpage.vue')
+    },
+    {
+      path: '/forgotpassword',
+      name: 'forgotpassword',
+      component: () => import('../views/ForPassword.vue')
+    },
+  ]
+})
+
+export default router
