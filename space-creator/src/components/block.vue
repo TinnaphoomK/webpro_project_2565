df096823ee014ae55be8af7cfee0e467a5225fcc
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-03-02 02:59:26 +0700

    block component

---
+++ space-creator/src/components/block.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bg-white shadow-5 w-8 h-6rem justify-content-center text-center text-xl align-items-center border-round-2xl"
+        style="margin-left: 17%; margin-top: 1%;padding-top: 1.7%;">
+        <div class="absolute w-10rem h-3rem text-white pt-2 border-round-right-2xl shadow-5 his"
+            style="background-color:rgb(15, 120, 19);">อนุมัติ</div>
+        Auditorium &emsp;&emsp;&emsp;&emsp; 13 FEB,2023 &emsp;&emsp;&emsp;&emsp; 11:00 - 13:00 &emsp;&emsp;&emsp;&emsp;
+        #10001
+        <a href=""><i class="absolute pi pi-ellipsis-h" style="margin-left: 13%;"></i></a>
+    </div>
+</template>
