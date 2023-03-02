9d330bc762b08a404e875f7d1671d4ecb415871d
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-03-02 15:11:32 +0700

    new

--- space-creator/src/components/block.vue
+++ space-creator/src/components/block.vue
@@ -1,42 +1,20 @@
 <template>
-    <div class="bg-white shadow-5 w-8 h-6rem justify-content-center text-center text-xl align-items-center border-round-2xl"
-        style="margin-left: 17%; margin-top: 1%;padding-top: 1.7%;">
-        <div class="absolute w-10rem h-3rem text-white pt-2 border-round-right-2xl shadow-5 his"
-            style="background-color:rgb(15, 120, 19);">อนุมัติ</div>
-        {{ roomNames }} &emsp;&emsp;&emsp;&emsp; {{ reservation.startdate }} &emsp;&emsp;&emsp;&emsp; {{ reservation.starttime }} - {{ reservation.endtime }} &emsp;&emsp;&emsp;&emsp;
-        #{{ reservation.id }}
-        <a href=""><i class="absolute pi pi-ellipsis-h" style="margin-left: 13%;"></i></a>
+  <div
+    class="bg-white shadow-5 w-8 h-6rem justify-content-center text-center text-xl align-items-center border-rounded-2xl"
+    style="margin-left: 17%; margin-top: 1%; padding-top: 1.7%">
+    <div class="absolute w-10rem h-3rem text-white pt-2 border-rounded-right-2xl shadow-5 his"
+      style="background-color: rgb(15, 120, 19)">
+      อนุมัติ
     </div>
+    {{ roomNames }} &emsp;&emsp;&emsp;&emsp; {{ startdate }} -
+    {{ enddate }} &emsp;&emsp;&emsp;&emsp; {{ starttime }} - {{ endtime }} &emsp;&emsp;&emsp;&emsp;
+    #{{ id }}
+    <a href=""><i class="absolute pi pi-ellipsis-h" style="margin-left: 13%"></i></a>
+  </div>
 </template>
 
 <script>
 export default {
-  props: {
-    reservation: Object // pass reservation object as a prop
-  },
-  data() {
-  return {
-    reservations: [],
-    startdate: '',
-    enddate: '',
-    starttime: '',
-    endtime: '',
-    value: '',
-    id: 1, // initialize id to 1
-    roomNames: [],
-    blockList: [], // add new property to store list of blocks
-  }
-},
-methods: {
-  // ...existing methods...
-  addBlock() {
-    this.blockList.push({});
-  },
-},
-
-  created() {
-    const roomNames = JSON.parse(localStorage.getItem('roomNames')) || [];
-    this.roomNames = roomNames;
-  }
+  props: ['roomNames', 'startdate', 'starttime', 'enddate', 'endtime', 'id']
 }
 </script>
--- space-creator/src/views/History.vue
+++ space-creator/src/views/History.vue
@@ -1,26 +1,52 @@
-<script>
-import Navbar from '../components/Navbar.vue';
-import block from '../components/block.vue';
-
-export default {
-    components: {
-        Navbar,
-        block
-    },
-    
-}
-</script>
-
 <template>
+  <div>
     <Navbar></Navbar>
     <!-- badge -->
     <div class="his bg-white shadow-5 w-2 h-6rem justify-content-center text-center text-5xl align-items-center border-round-2xl"
         style="margin-left: 42%; margin-top: 3%;padding-top: 1%;">
         ประวัติการจอง
-    </div>   
-    
-
+    </div>
+    <block v-for="reservation in reservations" :key="reservation.id" :roomName="reservation.roomName" :startdate="reservation.startdate" :enddate="reservation.enddate" :starttime="reservation.starttime" :endtime="reservation.endtime" :id="reservation.id"></block>
+  </div>
 </template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+import block from '../components/block.vue';
+
+export default {
+  components: {
+    Navbar,
+    block
+  },
+  data() {
+    return {
+      reservations: [],
+      reservation: {
+        startdate: '',
+        starttime: '',
+        enddate: '',
+        endtime: '',
+        id: ''
+      },
+      roomNames: [],
+      roomName: {
+        roomName: ''
+      }
+    }
+  },
+  created() {
+    const storedReservations = localStorage.getItem('reservations');
+    if (storedReservations) {
+      this.reservations = JSON.parse(storedReservations);
+    }
+    const storedRoomNames = localStorage.getItem('roomNames');
+    if (storedRoomNames) {
+      this.roomNames = JSON.parse(storedRoomNames);
+    }
+  }
+}
+</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
