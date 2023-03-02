bb75abebe3a310191072336e1bf0dd80b3b2a426
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-03-02 16:19:24 +0700

    report button component

---
+++ space-creator/src/components/ReportRpending.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="bg-white shadow-5 w-8 h-6rem justify-content-center text-center text-xl align-items-center border-round-2xl"
+        style="margin-left: 17%; margin-top: 1%;padding-top: 1.7%;">
+        <div class="absolute w-10rem h-3rem text-white pt-2 border-round-right-2xl shadow-5 his"
+            style="background-color:rgb(35, 87, 165);">รอดำเนินการ</div>
+        &emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;RoomId &emsp;&emsp;&emsp;&emsp; Auditorium &emsp;&emsp;&emsp;&emsp;
+        เครื่องเสียมีปัญหา
+        &emsp;&emsp;&emsp;&emsp;
+        #10000
+        <a href="">
+            <div class="inline-block w-7rem h-3rem text-center pt-2 text-white border-round-2xl shadow-5 his bg-green-700 hover:bg-green-800 hover:text-400"
+                style="margin-left: 10%;">เสร็จสิ้น</div>
+        </a>
+
+        <a href=""><i class="inline-block pi pi-ellipsis-h" style="margin-left: 7.5%;"></i></a>
+    </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mitr&display=swap');
+
+* {
+    font-family: 'Inter', sans-serif;
+    margin: 0;
+}
+
+.his {
+    font-family: 'Mitr', sans-serif;
+    color: rgb(35, 87, 165);
+}
+
+.bar {
+    height: 0.2vh;
+    width: 58vw;
+    /* Position the rectangle in the center */
+    position: absolute;
+    top: 33%;
+    left: 53%;
+    background-color: #9E9999;
+    transform: translate(-50%, -50%);
+}
+
+a:link {
+    text-decoration: none;
+}
+
+a:visited {
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: none;
+}
+
+a:active {
+    text-decoration: none;
+}
+</style>
