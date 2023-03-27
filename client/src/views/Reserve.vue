5c7a332236e58ac5503529622a6de57975ada325
thampaponn <64070046@it.kmitl.ac.th>
2023-03-27 08:49:25 +0700

    new reserve

--- client/src/views/Reserve.vue
+++ client/src/views/Reserve.vue
@@ -1,62 +1,36 @@
 <script>
-import Navbar from '../components/Navbar.vue';
-import reserve from '../components/reserve.json'
+import Navbar from '../components/Navbar.vue'
+import CardItem from '../components/CardItem.vue'
 
 export default {
     components: {
         Navbar,
-
-    },
-    data() {
-        return {
-            reservations: [],
-            startdate: '',
-            enddate: '',
-            starttime: '',
-            endtime: '',
-            value: '',
-            id: 1, // initialize id to 1
-            roomNames: []
-        }
-    },
-    created() {
-        const storedReservations = localStorage.getItem('reservations');
-        if (storedReservations) {
-            this.reservations = JSON.parse(storedReservations);
-        }
-        const storedRoomNames = localStorage.getItem('roomNames');
-        if (storedRoomNames) {
-            this.roomNames = JSON.parse(storedRoomNames);
-        }
+        CardItem
     },
     methods: {
-        saveInputs() {
+        saveRoomName() {
+            // Check if user is logged in
+            const isLoggedIn = localStorage.getItem('isLoggedIn');
 
-            const reservation = {
-                id: this.id,
-                startdate: this.startdate,
-                enddate: this.enddate,
-                starttime: this.starttime,
-                endtime: this.endtime,
-                value: this.value
-            };
-            const roomNames = {
-                roomNames: this.roomNames
+            if (isLoggedIn) {
+                // Get the current list of room names from local storage, or initialize it to an empty array if it doesn't exist yet
+                const roomNames = JSON.parse(localStorage.getItem('roomNames')) || [];
+
+                // Add the room name to the list
+                roomNames.push(document.getElementById('roomname').innerText);
+
+                // Save the updated list to local storage
+                localStorage.setItem('roomNames', JSON.stringify(roomNames));
+                this.$router.push('/reserve');
+                console.log('go to reserve')
+            } else {
+                // Display alert message
+                alert('Please login first');
+                this.$router.push('/signin');
             }
-            // increment id for next item
-            this.id++;
-            // add reservation to localStorage
-            localStorage.setItem(`reservation-${reservation.id}`, JSON.stringify(reservation));
-            localStorage.setItem(`{roomNames.roomNames}`, JSON.stringify(roomNames));
-
-
-            this.$router.push('/history');
-
-
-
         }
     }
-}
+};
 </script>
 
 <template>
@@ -68,116 +42,45 @@ export default {
         <router-link to="/detail" class="thai first text-gray-900 hover:text-primary-600">รายละเอียด</router-link>
         <router-link to="/reserve" class="thai ml-8 text-primary-600 hover:text-primary-600">รายการจอง</router-link>
 
-        <div class="card mx-8 mt-3 py-4 shadow-5 border-round-sm bg-white">
-            <div class="flex justify-content-center flex-wrap card-container">
-                <div class="flex grid justify-content-center">
-                    <div
-                        class="bg-blue-100 w-15rem h-7rem border-round-xl z-1 shadow-3 m-4 flex align-items-center justify-content-center">
-                        1
-                    </div>
-                    <div
-                        class="bg-blue-100 w-15rem h-7rem border-round-xl z-1 shadow-3 m-4 flex align-items-center justify-content-center">
-                        2
-                    </div>
-                    <div
-                        class="bg-blue-100 w-15rem h-7rem border-round-xl z-1 shadow-3 m-4 flex align-items-center justify-content-center">
-                        3
-                    </div>
-                    <div
-                        class="bg-blue-100 w-15rem h-7rem border-round-xl z-1 shadow-3 m-4 flex align-items-center justify-content-center">
-                        4
-                    </div>
+        <div class="card mx-8 mt-3 py-6 shadow-5 border-round-sm bg-white">
+            <div class="flex justify-content-around flex-wrap card-container">
+                <div class="flex align-items-center justify-content-center">
+                    <img src="../assets/img/auditorium.jpeg" class="w-30rem h-30rem border-round-2xl my-4" alt="">
                 </div>
-                <div class="flex justify-content-center align-items-center flex-wrap card-container mt-2">
-                    <div class="flex align-items-center justify-content-center w-5 mx-4">
+
+                <div class="flex card-container mt-3 mx-6 justify-content-start">
+                    <div>
                         <label for="startdate" class="thai text-xl">วันที่เริ่มจอง</label>
                         <InputText id="startdate" v-model="startdate" name="startdate" type="date"
-                            class="p-inputtext-lg shadow-2 mx-4 w-8" />
+                            class="p-inputtext-lg shadow-2 w-full" />
                     </div>
-                    <div class="flex align-items-center justify-content-center w-5 mx-4">
+
+                    <div>
                         <label for="enddate" class="thai text-xl">สิ้นสุดการจอง</label>
                         <InputText id="enddate" v-model="enddate" name="enddate" type="date"
-                            class="p-inputtext-lg shadow-2 mx-4 w-8" />
+                            class="p-inputtext-lg shadow-2 w-full" />
                     </div>
-                    <div class="flex align-items-center justify-content-center w-5 mx-4 mt-4">
+
+                    <div>
                         <label for="starttime" class="thai text-xl">ตั้งแต่เวลา</label>
                         <InputText id="starttime" v-model="starttime" name="starttime" type="time"
-                            class="p-inputtext-lg shadow-2 mx-4 w-8" />
+                            class="p-inputtext-lg shadow-2 w-full" />
                     </div>
 
-                    <div class="flex align-items-center justify-content-center w-5 mx-3 mt-4">
-                        <label for="endtime" class="thai text-xl ml-6">ถึงเวลา</label>
+                    <div>
+                        <label for="endtime" class="thai text-xl">ถึงเวลา</label>
                         <InputText id="endtime" v-model="endtime" name="endtime" type="time"
-                            class="p-inputtext-lg shadow-2 mx-4 w-8" />
+                            class="p-inputtext-lg shadow-2 w-full" />
                     </div>
+
+                    <Button @click="saveRoomName"
+                        class="thai bg-primary-800 hover:bg-primary-900 border-round-xl text-xl absolute w-12rem h-4rem justify-content-center shadow-5 mt-8">ยืนยันการจอง</Button>
                 </div>
-                <Textarea placeholder="หมายเหตุ ..." class="flex align-items-top w-full h-10rem mt-5 mx-8"></Textarea>
-                <Button @click="saveInputs"
-                    class="thai bg-primary-800 hover:bg-primary-900 border-round-xl text-xl w-12rem h-4rem justify-content-center mt-4">ยืนยันการจอง</Button>
             </div>
         </div>
-
 
     </div>
 </template>
-
-<!-- <template>
-    <Navbar></Navbar>
-
-    <div class="relative text-left justify-text-center font-bold text-2xl mt-6">
-        <router-link to="/detail" class="thai first text-primary-600 hover:text-primary-600">รายละเอียด</router-link>
-        <router-link to="/reserve" class="thai ml-8 text-gray-900 hover:text-primary-600">รายการจอง</router-link>
-
-        <div class="cardbg mt-4 shadow-5">
-
-            <div class="justify-content-center">
-                <div class="grid justify-content-center">
-                    <div class="cardone bg-primary w-20rem h-7rem border-round-2xl z-1 shadow-6">
-                        <div class="absolute border-round-3xl" style="margin-left: 14.5%; margin-top: 10%;"></div>
-                    </div>
-                    <div class="cardone bg-primary w-20rem h-7rem border-round-2xl z-1 shadow-6">
-                        <div class="absolute border-round-3xl" style="margin-left: 14.5%; margin-top: 10%;"></div>
-                    </div>
-                    <div class="cardone bg-primary w-20rem h-7rem border-round-2xl z-1 shadow-6">
-                        <div class="absolute border-round-3xl" style="margin-left: 14.5%; margin-top: 10%;"></div>
-                    </div>
-                    <div class="cardone bg-primary w-20rem h-7rem border-round-2xl z-1 shadow-6">
-                        <div class="absolute border-round-3xl" style="margin-left: 14.5%; margin-top: 10%;"></div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="ml-6 mt-4">
-                <label for="startdate" class="thai text-xl ml-8">วันที่เริ่มจอง</label>
-                <InputText id="startdate" v-model="startdate" name="startdate" type="date"
-                    class="p-inputtext-lg shadow-2 mr-8 ml-4 mt-2 mb-4" style="width: 30%;" />
-
-                <label for="enddate" class="thai text-xl ml-8">วันที่เริ่มจอง</label>
-                <InputText id="enddate" v-model="enddate" name="enddate" type="date"
-                    class="p-inputtext-lg shadow-2 mr-8 ml-4 mt-2 mb-4" style="width: 30%;" />
-            </div>
-            <div class="ml-6 mt-4">
-                <label for="starttime" class="thai text-xl ml-8 mr-6">ตั้งแต่</label>
-                <InputText id="starttime" v-model="starttime" name="starttime" type="time"
-                    class="p-inputtext-lg shadow-2 mr-8 ml-4 mt-2" style="width: 30%;" />
-
-                <label for="endtime" class="thai text-xl ml-8 mr-3">ถึงเวลา</label>
-                <InputText id="endtime" v-model="endtime" name="endtime" type="time"
-                    class="p-inputtext-lg shadow-2 mr-8 ml-6 mt-2 mb-6" style="width: 30%;" />
-            </div>
-
-            <label for="comment" class="absolute thai text-xl" style="margin-left: 6%;">หมายเหตุ</label>
-            <div class="text-center">
-                <Textarea v-model="value" class="shadow-5" style="margin-left: 10%; margin-bottom: 3.2%;" rows="5"
-                    cols="136" />
-            </div>
-
-            <Button @click="saveInputs" class="thai border-round-xl text-xl absolute w-12rem h-3rem justify-content-center"
-                style="right: 45%; top: 90%; background-color: rgb(35, 87, 165);">ยืนยันการจอง</Button>
-        </div>
-
-    </div>
-</template> -->
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
---
+++ client/src/views/ReserveTable.vue
@@ -0,0 +1,219 @@
+<script lang="ts">
+import Navbar from '../components/Navbar.vue';
+import reserve from '../components/reserve.json'
+
+export default {
+    components: {
+        Navbar,
+
+    },
+    data() {
+        return {
+            reservations: [],
+            startdate: '',
+            enddate: '',
+            starttime: '',
+            endtime: '',
+            value: '',
+            id: 1, // initialize id to 1
+            roomNames: []
+        }
+    },
+    created() {
+        const storedReservations = localStorage.getItem('reservations');
+        if (storedReservations) {
+            this.reservations = JSON.parse(storedReservations);
+        }
+        const storedRoomNames = localStorage.getItem('roomNames');
+        if (storedRoomNames) {
+            this.roomNames = JSON.parse(storedRoomNames);
+        }
+    },
+    methods: {
+        saveInputs() {
+
+            const reservation = {
+                id: this.id,
+                startdate: this.startdate,
+                enddate: this.enddate,
+                starttime: this.starttime,
+                endtime: this.endtime,
+                value: this.value
+            };
+            const roomNames = {
+                roomNames: this.roomNames
+            }
+            // increment id for next item
+            this.id++;
+            // add reservation to localStorage
+            localStorage.setItem(`reservation-${reservation.id}`, JSON.stringify(reservation));
+            localStorage.setItem(`{roomNames.roomNames}`, JSON.stringify(roomNames));
+
+
+            this.$router.push('/history');
+
+
+
+        }
+    }
+}
+</script>
+
+<template>
+    <!-- navbar -->
+    <Navbar></Navbar>
+
+    <!-- top bar-->
+    <div class="relative text-left justify-text-center font-bold text-2xl mt-6">
+        <router-link to="/detail" class="thai first text-gray-900 hover:text-primary-600">รายละเอียด</router-link>
+        <router-link to="/reserve" class="thai ml-8 text-primary-600 hover:text-primary-600">รายการจอง</router-link>
+
+        <div class="card mx-8 mt-3 py-4 shadow-5 border-round-sm bg-white">
+            <div class="flex justify-content-center flex-wrap card-container">
+                <div class="flex grid justify-content-center">
+                    <div
+                        class="bg-blue-100 w-15rem h-7rem border-round-xl z-1 shadow-3 m-4 flex align-items-center justify-content-center">
+                        1
+                    </div>
+                    <div
+                        class="bg-blue-100 w-15rem h-7rem border-round-xl z-1 shadow-3 m-4 flex align-items-center justify-content-center">
+                        2
+                    </div>
+                    <div
+                        class="bg-blue-100 w-15rem h-7rem border-round-xl z-1 shadow-3 m-4 flex align-items-center justify-content-center">
+                        3
+                    </div>
+                    <div
+                        class="bg-blue-100 w-15rem h-7rem border-round-xl z-1 shadow-3 m-4 flex align-items-center justify-content-center">
+                        4
+                    </div>
+                </div>
+                <div class="flex justify-content-center align-items-center flex-wrap card-container mt-2">
+                    <div class="flex align-items-center justify-content-center w-5 mx-4">
+                        <label for="startdate" class="thai text-xl">วันที่เริ่มจอง</label>
+                        <InputText id="startdate" v-model="startdate" name="startdate" type="date"
+                            class="p-inputtext-lg shadow-2 mx-4 w-8" />
+                    </div>
+                    <div class="flex align-items-center justify-content-center w-5 mx-4">
+                        <label for="enddate" class="thai text-xl">สิ้นสุดการจอง</label>
+                        <InputText id="enddate" v-model="enddate" name="enddate" type="date"
+                            class="p-inputtext-lg shadow-2 mx-4 w-8" />
+                    </div>
+                    <div class="flex align-items-center justify-content-center w-5 mx-4 mt-4">
+                        <label for="starttime" class="thai text-xl">ตั้งแต่เวลา</label>
+                        <InputText id="starttime" v-model="starttime" name="starttime" type="time"
+                            class="p-inputtext-lg shadow-2 mx-4 w-8" />
+                    </div>
+
+                    <div class="flex align-items-center justify-content-center w-5 mx-3 mt-4">
+                        <label for="endtime" class="thai text-xl ml-6">ถึงเวลา</label>
+                        <InputText id="endtime" v-model="endtime" name="endtime" type="time"
+                            class="p-inputtext-lg shadow-2 mx-4 w-8" />
+                    </div>
+                </div>
+                <Textarea placeholder="หมายเหตุ ..." class="flex align-items-top w-full h-10rem mt-5 mx-8"></Textarea>
+                <Button @click="saveInputs"
+                    class="thai bg-primary-800 hover:bg-primary-900 border-round-xl text-xl w-12rem h-4rem justify-content-center mt-4">ยืนยันการจอง</Button>
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<!-- <template>
+    <Navbar></Navbar>
+
+    <div class="relative text-left justify-text-center font-bold text-2xl mt-6">
+        <router-link to="/detail" class="thai first text-primary-600 hover:text-primary-600">รายละเอียด</router-link>
+        <router-link to="/reserve" class="thai ml-8 text-gray-900 hover:text-primary-600">รายการจอง</router-link>
+
+        <div class="cardbg mt-4 shadow-5">
+
+            <div class="justify-content-center">
+                <div class="grid justify-content-center">
+                    <div class="cardone bg-primary w-20rem h-7rem border-round-2xl z-1 shadow-6">
+                        <div class="absolute border-round-3xl" style="margin-left: 14.5%; margin-top: 10%;"></div>
+                    </div>
+                    <div class="cardone bg-primary w-20rem h-7rem border-round-2xl z-1 shadow-6">
+                        <div class="absolute border-round-3xl" style="margin-left: 14.5%; margin-top: 10%;"></div>
+                    </div>
+                    <div class="cardone bg-primary w-20rem h-7rem border-round-2xl z-1 shadow-6">
+                        <div class="absolute border-round-3xl" style="margin-left: 14.5%; margin-top: 10%;"></div>
+                    </div>
+                    <div class="cardone bg-primary w-20rem h-7rem border-round-2xl z-1 shadow-6">
+                        <div class="absolute border-round-3xl" style="margin-left: 14.5%; margin-top: 10%;"></div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="ml-6 mt-4">
+                <label for="startdate" class="thai text-xl ml-8">วันที่เริ่มจอง</label>
+                <InputText id="startdate" v-model="startdate" name="startdate" type="date"
+                    class="p-inputtext-lg shadow-2 mr-8 ml-4 mt-2 mb-4" style="width: 30%;" />
+
+                <label for="enddate" class="thai text-xl ml-8">วันที่เริ่มจอง</label>
+                <InputText id="enddate" v-model="enddate" name="enddate" type="date"
+                    class="p-inputtext-lg shadow-2 mr-8 ml-4 mt-2 mb-4" style="width: 30%;" />
+            </div>
+            <div class="ml-6 mt-4">
+                <label for="starttime" class="thai text-xl ml-8 mr-6">ตั้งแต่</label>
+                <InputText id="starttime" v-model="starttime" name="starttime" type="time"
+                    class="p-inputtext-lg shadow-2 mr-8 ml-4 mt-2" style="width: 30%;" />
+
+                <label for="endtime" class="thai text-xl ml-8 mr-3">ถึงเวลา</label>
+                <InputText id="endtime" v-model="endtime" name="endtime" type="time"
+                    class="p-inputtext-lg shadow-2 mr-8 ml-6 mt-2 mb-6" style="width: 30%;" />
+            </div>
+
+            <label for="comment" class="absolute thai text-xl" style="margin-left: 6%;">หมายเหตุ</label>
+            <div class="text-center">
+                <Textarea v-model="value" class="shadow-5" style="margin-left: 10%; margin-bottom: 3.2%;" rows="5"
+                    cols="136" />
+            </div>
+
+            <Button @click="saveInputs" class="thai border-round-xl text-xl absolute w-12rem h-3rem justify-content-center"
+                style="right: 45%; top: 90%; background-color: rgb(35, 87, 165);">ยืนยันการจอง</Button>
+        </div>
+
+    </div>
+</template> -->
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mitr&display=swap');
+
+
+* {
+    font-family: 'Inter', sans-serif;
+    margin: 0;
+}
+
+body {
+    background-color: rgba(35, 87, 165, 0.1);
+}
+
+.thai {
+    font-family: 'Mitr', sans-serif;
+}
+
+.first {
+    margin-left: 10%;
+}
+
+a:link {
+    text-decoration: none;
+}
+
+a:visited {
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: none;
+}
+
+a:active {
+    text-decoration: none;
+}
+</style>
