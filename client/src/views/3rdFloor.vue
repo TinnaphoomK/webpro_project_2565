e08e969a4259706b32d1f19fc364f2bc28af84a5
thampaponn <64070046@it.kmitl.ac.th>
2023-03-29 16:31:43 +0700

    fixing signin signup desktop responsive

--- client/src/views/3rdFloor.vue
+++ client/src/views/3rdFloor.vue
@@ -1,21 +1,32 @@
+<script>
+import Navbar from '../components/Navbar.vue'
+import CardItem from '../components/CardItem.vue'
+export default {
+    components: {
+        Navbar,
+        CardItem
+    },
+};
+</script>
+
 <template>
     <!-- navbar -->
     <Navbar></Navbar>
-
     <!-- search bar -->
-    <div class="pt-4 text-center w-full flex justify-content-center align-items-center">
-
-        <InputText placeholder="Search here!" id="search" type="text" class="p-inputtext-lg shadow-2" style="width: 50%;"
-            v-model="value" />
+    <div class="pt-4 mt-4 text-center w-full flex justify-content-center align-items-center">
+        <span class="p-float-label">
+            <InputText id="search" type="text" class="p-inputtext-lg shadow-2 w-30rem" v-model="value" />
+            <label for="search">Search here !</label>
+        </span>
 
     </div>
 
     <!-- 1st floor -->
     <div class="relative text-left justify-text-center font-bold text-2xl pt-6">
-        <router-link to="/" class="text-gray-900 ml-8 hover:text-primary-800">1ST FLOOR</router-link>
+        <router-link to="/" class="text-primary-600 ml-8">1ST FLOOR</router-link>
         <router-link to="/mfloor" class="text-gray-900 ml-6 hover:text-primary-800">M FLOOR</router-link>
         <router-link to="/2ndfloor" class="text-gray-900 ml-6 hover:text-primary-800">2ND FLOOR</router-link>
-        <router-link to="/3rdfloor" class="text-primary-600 ml-6">3RD FLOOR</router-link>
+        <router-link to="/3rdfloor" class="text-gray-900 ml-6 hover:text-primary-800">3RD FLOOR</router-link>
 
         <div class="mx-7 my-2 px-2 py-8 border-round-xl shadow-5" style="background-color: rgba(35, 87, 165, 0.1);">
 
@@ -52,7 +63,41 @@
             </div>
 
         </div>
+        <div class="flex justify-content-center mx-7 my-6 px-2 py-4 border-round-xl shadow-5"
+            style="background-color: rgba(35, 87, 165, 0.1);">
+            <div class="flex flex-column card-container text-lg mx-6 my-6 justify-content-start align-self-start">
+                <label class="flex flex-column w-30rem my-3 bg-transparent font-bold text-5xl text-gray-900 border-round">
+                    Auditorium Room</label>
+                <label class="my-4" for="">For the event and meeting for each important things in faculty. Exclusive</label>
+                <ul class="flex flex-column list-none align-items-start">
+                    <li class="flex my-2 align-items-center">
+                        <img class="mx-2" src="../assets/img/itcirclelogo.png" alt="">
+                        220 seats
+                    </li>
+                    <li class="flex my-2 align-items-center">
+                        <img class="mx-2" src="../assets/img/itcirclelogo.png" alt="">
+                        Wide screen projector
+                    </li>
+                    <li class="flex my-2 align-items-center">
+                        <img class="mx-2" src="../assets/img/itcirclelogo.png" alt="">
+                        Stage Setup
+                    </li>
+                    <li class="flex my-2 align-items-center">
+                        <img class="mx-2" src="../assets/img/itcirclelogo.png" alt="">
+                        Sound Control room
+                    </li>
+                </ul>
+            </div>
+            <div class="flex text-lg my-8 mx-6 justify-content-center align-items-center"><img
+                    src="/src/assets/img/auditorium.jpeg" class="w-full h-full border-round-2xl my-4 mx-8" alt="">
+            </div>
+        </div>
 
+    </div>
+    <div
+        class="flex footerbg bottom-0 w-full h-3rem text-white text-sm align-items-center justify-content-center text-center">
+        <img src="../assets/img/cc.png" class="bg-transparent mx-2" style="width: 0.9%;" alt=""> All Right Reserved |
+        Space Creator
     </div>
 </template>
 
@@ -66,6 +111,10 @@
 
 .first {
     margin-left: 6%;
+}
+
+.footerbg {
+    background-image: linear-gradient(to right, rgb(3, 8, 16), rgb(35, 87, 165), rgb(3, 8, 16));
 }
 
 a:link {
@@ -85,13 +134,3 @@ a:active {
 }
 </style>
 
-<script>
-import Navbar from '../components/Navbar.vue'
-import CardItem from '../components/CardItem.vue'
-export default {
-    components: {
-        Navbar,
-        CardItem
-    },
-};
-</script>
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,148 +1,89 @@
 <template>
-  <!-- login -->
-  <div class="rectangle">
-    <div class="blue-half border-round-left-2xl shadow-5">
-      <h1 class="bg-transparent text-white text-center">SPACE CREATOR</h1>
-      <div class="bar"></div>
-      <h2 class="bg-transparent text-yellow-400 text-center mt-6">your space, your choice</h2>
-      <center class="bg-transparent">
-        <img class="itpic border-round-xl" src="../assets/img/itkmitl.jpeg" alt="">
-      </center>
-    </div>
-    <div class="white-half border-round-right-2xl shadow-5">
-      <form class="absolute">
+  <!-- navbar -->
+  <Navbar></Navbar>
 
-        <label for="username" class="text-xl mx-7">Username</label>
-        <InputText id="username" v-model="username" placeholder="Fill your username" name="username" type="text"
-          class="p-inputtext-lg shadow-2 mx-7" style="width: 75%;" />
-        <br>
-        <label for="password" class="text-xl mx-7">Password</label>
-        <InputText id="password" v-model="password" placeholder="Set your password" name="password" type="password"
-          class="p-inputtext-lg shadow-2 mx-7" style="width: 75%;" />
-        <br><br>
+  <!-- top bar-->
+  <div class="flex justify-content-center align-items-center h-screen">
+    <div class="flex justify-content-center">
+      <div
+        class="flex flex-column align-items-center justify-content-center w-30rem h-30rem p-8 bglinear border-round-left-xl shadow-5">
+        <img class="flex flex-column w-5 my-4" src="../assets/img/logo.png" alt="">
+        <label class="flex flex-column bg-transparent text-4xl font-bold text-white" for="">SPACE CREATOR</label>
+        <div class="flex flex-column bar bg-white mb-2"></div>
+        <label class="flex flex-column bg-transparent text-lg font-bold text-yellow-400" for="">your space, your
+          choice</label>
+      </div>
+      <div
+        class="flex flex-column align-items-center justify-content-center w-30rem h-30rem bg-white font-normal text-white border-round-right-xl shadow-5">
+        <form class="flex flex-column w-full justify-content-center" action="">
 
-        <router-link to="/signin">
-          <Button @click='signup()' class="absolute text-center justify-content-center text-bold text-xl mx-7"
-            style="width: 70%; height: 12%;background-image: linear-gradient(to right, rgb(3, 8, 16), rgb(35, 87, 165));">Sign
-            up</Button><br><br><br><br><br>
-        </router-link>
+          <div class="my-2">
+            <label class="flex text-black-alpha-90 justify-content-start mx-4" for="email">Email</label>
+            <div class="flex">
+              <InputText class="flex w-full shadow-1 mx-4 mt-1" id="email" v-model="value" />
+            </div>
+          </div>
 
-        <label for="password" class="text-center">already have an account ?<a class="bg-transparent" href="/signin"> Sign
-            in</a></label>
-      </form>
+          <div class="my-2">
+            <label class="flex text-black-alpha-90 justify-content-start mx-4" for="password">Password</label>
+            <div class="flex">
+              <InputText class="flex w-full shadow-1 mx-4 mt-1" id="password" v-model="value" />
+            </div>
+          </div>
+          <Button @click.prevent="signin()"
+            class="flex bg-primary-800 text-white hover:bg-primary-900 hover:text-gray-300 justify-content-center text-bold shadow-1 my-4 mx-4">Sign
+            in</Button>
+          <router-link to="/forgotpassword"
+            class="flex text-sm text-primary-700 hover:text-primary-900 justify-content-end bg-transparent mx-4">
+            Forget password ?
+          </router-link>
+          <div class="flex justify-content-center mt-4">
+            <label for="password" class="flex justify-content-center text-gray-500 text-sm">don't have an account yet
+              ?</label>
+            <router-link class="flex text-sm text-primary-700 hover:text-primary-900 mx-2" to="/signup">Sign up
+            </router-link>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 
-  <!-- footer -->
-  <div class="footer h-3rem fixed text-white text-center pt-3">
-    <img src="../assets/img/cc.png" class="bg-transparent" style="width: 0.9%;" alt=""> All Right Reserved | Space Creator
+  <div
+    class="flex footerbg absolute bottom-0 w-full h-3rem text-white text-sm align-items-center justify-content-center text-center">
+    <img src="../assets/img/cc.png" class="bg-transparent mx-2" style="width: 0.9%;" alt=""> All Right Reserved |
+    Space Creator
   </div>
 </template>
 
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mitr&display=swap');
 
 * {
-  background-color: rgb(213, 219, 231);
   font-family: 'Inter', sans-serif;
   margin: 0;
 }
 
-h1 {
-  font-size: 3rem;
-  margin-top: 15%;
-}
-
-h2 {
-  font-size: 1.5rem;
-  margin-top: 3%;
-  margin-bottom: 4%;
-}
-
-.centered-element {
-  margin-top: 10%;
-  position: absolute;
-}
-
-.footer {
-  background-image: linear-gradient(to right, rgb(3, 8, 16), rgb(35, 87, 165), rgb(3, 8, 16));
-  bottom: 0;
-  width: 100%;
-}
-
-.rectangle {
-  height: 75vh;
-  width: 75vw;
-  /* Position the rectangle in the center */
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.itpic {
-  width: 90%;
-  height: 100%;
-  object-fit: cover;
-  margin-top: 10%;
+body {
+  background-color: rgba(35, 87, 165, 0.1);
 }
 
 .bar {
-  height: 0.7vh;
-  width: 30vw;
-  /* Position the rectangle in the center */
-  position: absolute;
-  top: 25%;
-  left: 25%;
-  background-color: white;
-  transform: translate(-50%, -50%);
+  height: 2%;
+  width: 100%;
 }
 
-/* Set the blue and white split color scheme */
-.blue-half {
-  height: 100%;
-  width: 50%;
-  float: left;
-  background-image: linear-gradient(to right, rgb(3, 8, 16), rgb(35, 87, 165));
+.thai {
+  font-family: 'Mitr', sans-serif;
 }
 
-.white-half {
-  height: 100%;
-  width: 50%;
-  float: right;
-  background-color: white;
+.footerbg {
+  background-image: linear-gradient(to right, rgb(3, 8, 16), rgb(35, 87, 165), rgb(3, 8, 16));
 }
 
-/* Style the form */
-form {
-  width: 50%;
-  margin: 0 auto;
-  margin-top: 6%;
-  padding: 20px;
-  background-color: white;
-  border-radius: 5px;
-}
-
-label,
-input {
-  display: block;
-  position: relative;
-  margin-bottom: 10px;
-  background-color: white;
-}
-
-input[type="submit"] {
-  background-color: blue;
-  color: white;
-  border: none;
-  padding: 10px;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-a {
-  color: rgb(35, 87, 165);
+.bglinear {
+  background-image: linear-gradient(to right, rgb(3, 8, 16), rgb(35, 87, 165))
 }
 
 a:link {
