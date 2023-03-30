42fcd36dc0da296ee464fc776bd172eb9084f9f8
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-03-31 00:11:10 +0700

    123

--- server/src/auth.js
+++ server/src/auth.js
@@ -1,63 +1,100 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import axios from "axios";
+import bcrypt from "bcrypt";
+import { defineStore } from "pinia";
+
 const prisma = new PrismaClient();
 const router = express.Router();
 
-
-router.get('/user', async (req,res) => {
-    try{
-        const user = await prisma.user.findMany();
-        res.status(200).json(user);
-    }
-    catch (error){
-        res.status(400).json({ error: error.message});
-    }
-});
-
-
-
-router.post('/login', async (req,res) => {
-    try{
-        const { email, password } = req.body;
-        const findEmail = await prisma.user.findUnique({
-            where:{
-                email:email,
-            }
-        })
-        if (!findEmail){ throw new Error("Email not found")};
-        if (findEmail.password != password){
-            throw new Error ("Password is incorrect")
-        }
-        res.status(200).json(findEmail());
-    } catch (error){
-        res.status(400).json({ error: error.message});
-    }
-});
-
-router.post('/register', async (req,res) =>{
-    try{
-        const {email, password, firstName, lastName, studentId } = req.body;
-        const findEmail = await prisma.user.findUnique({
-            where:{
-                email:email,
-            }
-        })
-        if (findEmail){ throw new Error("Email already exist");}
-        const user = await prisma.user.create({
-            data:{
-                email: email,
-                password: password,
-                firstName: firstName,
-                lastName: lastName,
-                studentId: studentId,
-            },
+// Define Pinia store for authentication
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    user: null,
+    token: null,
+  }),
+  actions: {
+    async login(email, password) {
+      try {
+        const response = await axios.post("/login", { email, password });
+        const { user, token } = response.data;
+        this.user = user;
+        this.token = token;
+        return user;
+      } catch (error) {
+        throw new Error(error.response.data.error);
+      }
+    },
+    async register(email, password, firstName, lastName, studentId) {
+      try {
+        const response = await axios.post("/register", {
+          email,
+          password,
+          firstName,
+          lastName,
+          studentId,
         });
-        res.status(200).json(user);
-    }catch (error){
-        console.log(error)
-        res.status(400).json({ error: error.message});
-    }
+        const { user, token } = response.data;
+        this.user = user;
+        this.token = token;
+        return user;
+      } catch (error) {
+        throw new Error(error.response.data.error);
+      }
+    },
+    logout() {
+      this.user = null;
+      this.token = null;
+    },
+  },
 });
 
+// Login route
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      return res.status(404).json({ error: "Email not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Incorrect password" });
+    }
+    const token = "fake_token"; // Replace with real token generation
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-export default router
+// Registration route
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, firstName, lastName, studentId } = req.body;
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      return res.status(409).json({ error: "Email already in use" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        studentId,
+      },
+    });
+    const token = "fake_token"; // Replace with real token generation
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
