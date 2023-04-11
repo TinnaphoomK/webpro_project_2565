c6b8d54190e33d171c9525a3e41bd84e162ebf7d
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-04-11 18:05:55 +0700

    mainpage carditem fix

--- client/src/components/CardItem.vue
+++ client/src/components/CardItem.vue
@@ -1,3 +1,15 @@
 <template>
-    <div class="bg-primary w-15rem h-10rem border-round-2xl z-1 shadow-6 mx-3"></div>
+    <div>
+        <div class="card border-round-2xl shadow-5">
+            <div class="flex justify-content-start flex-wrap">
+                    <img class="flex w-18rem h-10rem border-round-top-2xl" src="../assets/img/auditorium.jpeg" alt="">
+            </div>
+            <div class="flex bg-white w-18rem h-4rem border-round-bottom-2xl">
+                <div class="flex justify-content-center p-4"> 
+                    <a href="" class="flex text-sm text-gray-900 mr-7">Auditorium</a>
+                    <i class="flex pi pi-search text-gray-900 text-2xl  align-items-center mx-8"></i>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
