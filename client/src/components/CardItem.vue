fffbfc2c145f0aceedc6fdfe16b2561865710039
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-04-11 17:53:34 +0700

    js

--- client/src/components/CardItem.vue
+++ client/src/components/CardItem.vue
@@ -1,3 +1,19 @@
 <template>
     <div class="bg-primary w-15rem h-10rem border-round-2xl z-1 shadow-6 mx-3"></div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {
+        room: {
+          name : "",
+          detail: "",
+          floor : ""
+        }
+      }
+    };
+  },
+}
+</script>
