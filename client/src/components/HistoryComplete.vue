27415bd6b3ce46392ae8a275ba31a1448ad379cd
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-05-12 11:07:15 +0700

    reservetable fix

--- client/src/components/HistoryComplete.vue
+++ client/src/components/HistoryComplete.vue
@@ -1,39 +1,3 @@
-<!-- <template>
-  <div class="card">
-    <div class="flex justify-content-center flex-wrap card-container">
-      <div v-for="(value, index) in reservation" :key="index"
-        class="bg-white text-primary-800 text-xl font-bold flex align-items-center justify-content-between w-full h-6rem border-round-2xl m-2 shadow-5">
-        
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import axios from 'axios';
-export default {
-    data() {
-    return {
-      reservation: {},
-    };
-  },
-  created: function () {
-    this.allreserve(this.roomId)
-  },
-  methods: {
-    async allreserve(roomId) {
-      const res = await axios.get('http://localhost:3000/api/reservation/')
-      console.log(res.data);
-
-      if (roomId) {
-        this.reservation = res.data.filter(reservation => reservation.roomId == this.roomId)
-      } else {
-        this.reservation = res.data
-      }
-    }
-  }
-}
-</script> -->
 <template>
   <div class="card">
     <div class="flex justify-content-center flex-wrap card-container">
@@ -42,9 +6,6 @@ export default {
         <div
           class="thai bg-green-700 font-normal text-lg text-white text-center align-items-center h-2rem w-7rem border-round-right-lg">
           อนุมัติ</div>
-        <!-- <div
-          class="thai bg-red-700 font-normal text-white text-center justify-content-center align-items-center h-2rem w-7rem mx-2">
-          ไม่อนุมัติ</div> -->
         <label class="thai ml-6" for="">ห้อง : {{ value.roomId }}</label>
         <label class="thai ml-6" for="">วันที่จอง : {{ value.dateTimeStart.slice(0, 10) }}</label>
         <label class="thai ml-6" for="">เวลาที่จอง : {{ value.dateTimeStart.slice(value.dateTimeStart.indexOf('T') + 1,
