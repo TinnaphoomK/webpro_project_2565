914c2f435ccd0573a62a56a461cbe8b44356d01c
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-05-16 20:40:58 +0700

    poi

--- client/src/components/ManageRoomClose.vue
+++ client/src/components/ManageRoomClose.vue
@@ -1,71 +1,92 @@
 <template>
     <!-- Show closed rooms -->
     <div class="card">
-        <div class="flex justify-content-center flex-wrap card-container">
-            <div
-                class="bg-white text-primary-800 text-xl font-bold flex align-items-center justify-content-between w-28rem h-6rem border-round-2xl mx-2 shadow-5">
-                <Button @click="changeStatus(room.id)"
-                    class="thai bg-red-700 hover:bg-red-800 text-sm h-2rem w-7rem justify-content-center ml-2">
-                    ปิดใช้งาน
-                </Button>
-                <label class="thai text-sm ml-2" for="">{{ room.name }}</label>
-                <label class="thai text-sm ml-2" for="">ชั้น : {{ room.floor }}</label>
-                <router-link to="/editroom">
-                    <i class="pi pi-ellipsis-h mx-2 text-xl text-900"></i>
-                </router-link>
-                <Button @click="deleteRoom(room.id)"
-                    class="flex justify-content-center thai text-sm bg-primary-700 h-2rem w-6rem hover:bg-primary-800 mx-2">
-                    ลบห้อง
-                </Button>
-            </div>
+      <div class="flex justify-content-center flex-wrap card-container">
+        <div
+          class="bg-white text-primary-800 text-xl font-bold flex align-items-center justify-content-between w-28rem h-6rem border-round-2xl mx-2 shadow-5"
+        >
+          <Button
+            @mouseenter="hover = true"
+            @mouseleave="hover = false"
+            @click="changeStatus(room.id)"
+            :class="[
+              'thai',
+              'text-sm',
+              'h-2rem',
+              'w-7rem',
+              'justify-content-center',
+              'ml-2',
+              hover ? 'bg-green-700 hover:bg-green-800' : 'bg-red-700 hover:bg-red-800',
+            ]"
+          >
+            {{ hover ? 'เปิดใช้งาน' : 'ปิดใช้งาน' }}
+          </Button>
+          <label class="thai text-sm ml-2" for="">{{ room.name }}</label>
+          <label class="thai text-sm ml-2" for="">ชั้น : {{ room.floor }}</label>
+          <router-link to="/editroom">
+            <i class="pi pi-ellipsis-h mx-2 text-xl text-900"></i>
+          </router-link>
+          <Button
+            @click="deleteRoom(room.id)"
+            class="flex justify-content-center thai text-sm bg-primary-700 h-2rem w-6rem hover:bg-primary-800 mx-2"
+          >
+            ลบห้อง
+          </Button>
         </div>
+      </div>
     </div>
-</template>
-
-<script>
-import axios from "axios";
-
-export default {
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
     props: {
-        room: Object,
+      room: Object
+    },
+    data() {
+      return {
+        hover: false
+      }
     },
     methods: {
-        async deleteRoom(id) {
-            console.log(id);
-            try {
-                if (id != null) {
-                    const res = await axios.delete(`http://localhost:3000/api/room/${id}`, {
-                        headers: {
-                            Authorization: "Bearer " + localStorage.getItem("token"),
-                        },
-                    });
-                    console.log(res.data);
-                    window.location.reload();
+      async deleteRoom(id) {
+        console.log(id);
+        try {
+          if (id != null) {
+            const res = await axios.delete(`http://localhost:3000/api/room/${id}`, {
+              headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+              }
+            });
+            console.log(res.data);
+            window.location.reload();
+          }
+        } catch (error) {
+          alert("ห้องนี้มีการจองอยู่");
+        }
+      },
+      async changeStatus(id) {
+        console.log(id);
+        try {
+          if (this.room.status === false) { // Check if the room status is closed
+            const res = await axios.put(
+              `http://localhost:3000/api/room/update/${id}`,
+              { status: true }, // Set the status to open
+              {
+                headers: {
+                  Authorization: "Bearer " + localStorage.getItem("token")
                 }
-            } catch (error) {
-                alert("ห้องนี้มีการจองอยู่");
-            }
-        },
-        async changeStatus(id) {
-            console.log(id);
-            try {
-                if (this.room.status === false) { // Check if the room status is closed
-                    const res = await axios.put(
-                        `http://localhost:3000/api/room/update/${id}`,
-                        { status: true }, // Set the status to open
-                        {
-                            headers: {
-                                Authorization: "Bearer " + localStorage.getItem("token"),
-                            },
-                        }
-                    );
-                    console.log(res.data);
-                    window.location.reload();
-                }
-            } catch (error) {
-                alert("ห้องนี้มีการจองอยู่");
-            }
-        },
-    },
-};
-</script>
+              }
+            );
+            console.log(res.data);
+            window.location.reload();
+          }
+        } catch (error) {
+          alert("ห้องนี้มีการจองอยู่");
+        }
+      }
+    }
+  };
+  </script>
+  
--- client/src/components/ManageRoomOpen.vue
+++ client/src/components/ManageRoomOpen.vue
@@ -1,68 +1,86 @@
 <template>
-    <!-- แสดงห้องที่  เปิด  ใช้งานอยู่ !! -->
+    <!-- แสดงห้องที่ เปิด ใช้งานอยู่ !! -->
     <div class="card">
-        <div class="flex justify-content-center flex-wrap card-container">
-            <div
-                class="bg-white text-primary-800 text-xl font-bold flex align-items-center justify-content-between w-28rem h-6rem border-round-2xl mx-2 shadow-5">
-                <Button @click="changeStatus(room.id)"
-                    class="thai bg-green-700 hover:bg-green-800 text-sm h-2rem w-7rem justify-content-center ml-2">
-                    เปิดใช้งาน
-                </Button>
-                <label class="thai text-sm ml-2" for="">{{ room.name }}</label>
-                <label class="thai text-sm ml-2" for="">ชั้น : {{ room.floor }}</label>
-                <router-link to="/editroom">
-                    <i class="pi pi-ellipsis-h mx-2 text-xl text-900"></i>
-                </router-link>
-                <Button @click="deleteRoom(room.id)"
-                    class="flex justify-content-center thai text-sm bg-primary-700 h-2rem w-6rem hover:bg-primary-800 mx-2">ลบห้อง</Button>
-            </div>
+      <div class="flex justify-content-center flex-wrap card-container">
+        <div class="bg-white text-primary-800 text-xl font-bold flex align-items-center justify-content-between w-28rem h-6rem border-round-2xl mx-2 shadow-5">
+          <Button
+            @mouseenter="hover = true"
+            @mouseleave="hover = false"
+            @click="changeStatus(room.id)"
+            :class="[
+              'thai',
+              'text-sm',
+              'h-2rem',
+              'w-7rem',
+              'justify-content-center',
+              'ml-2',
+              hover ? 'bg-red-700 hover:bg-red-800' : room.status ? 'bg-green-700 hover:bg-red-800' : 'bg-green-700 hover:bg-green-800'
+            ]"
+          >
+            {{ hover ? 'ปิดใช้งาน' : room.status ? 'เปิดใช้งาน' : 'เปิดใช้งาน' }}
+          </Button>
+          <label class="thai text-sm ml-2" for="">{{ room.name }}</label>
+          <label class="thai text-sm ml-2" for="">ชั้น : {{ room.floor }}</label>
+          <router-link to="/editroom">
+            <i class="pi pi-ellipsis-h mx-2 text-xl text-900"></i>
+          </router-link>
+          <Button @click="deleteRoom(room.id)" 
+          class="flex justify-content-center thai text-sm bg-primary-700 h-2rem w-6rem hover:bg-primary-800 mx-2">
+            ลบห้อง
+          </Button>
         </div>
+      </div>
     </div>
-</template>
-
-<script>
-import axios from 'axios'
-export default {
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  export default {
     props: {
-        room: Object
+      room: Object
+    },
+    data() {
+      return {
+        hover: false
+      }
     },
     methods: {
-        async deleteRoom(id) {
-            console.log(id)
-            try {
-                if (id != null) {
-                    const res = await axios.delete(`http://localhost:3000/api/room/${id}`, {
-                        headers: {
-                            Authorization: "Bearer " + localStorage.getItem("token")
-                        }
-                    })
-                    console.log(res.data)
-                    window.location.reload()
-                }
-            } catch (error) {
-                alert("ห้องนี้มีการจองอยู่")
+      async deleteRoom(id) {
+        console.log(id)
+        try {
+          if (id != null) {
+            const res = await axios.delete(`http://localhost:3000/api/room/${id}`, {
+              headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+              }
+            })
+            console.log(res.data)
+            window.location.reload()
+          }
+        } catch (error) {
+          alert("ห้องนี้มีการจองอยู่")
+        }
+      },
+      async changeStatus(id) {
+        console.log(id);
+        try {
+          const newStatus = !this.room.status; // Toggle the status
+          const res = await axios.put(
+            `http://localhost:3000/api/room/update/${id}`,
+            { status: newStatus }, // Set the new status
+            {
+              headers: {
+                Authorization: "Bearer " + localStorage.getItem("token"),
+              },
             }
-        },
-        async changeStatus(id) {
-            console.log(id);
-            try {
-                if (this.room.status === true) { // Check if the room status is open
-                    const res = await axios.put(
-                        `http://localhost:3000/api/room/update/${id}`,
-                        { status: false }, // Set the status to closed
-                        {
-                            headers: {
-                                Authorization: "Bearer " + localStorage.getItem("token"),
-                            },
-                        }
-                    );
-                    console.log(res.data);
-                    window.location.reload();
-                }
-            } catch (error) {
-                alert("ห้องนี้มีการจองอยู่");
-            }
-        },
+          );
+          console.log(res.data);
+          this.room.status = newStatus; // Update the room status locally
+        } catch (error) {
+          alert("ห้องนี้มีการจองอยู่");
+        }
+      },
     },
-}
-</script>
+  }
+  </script>
+  
