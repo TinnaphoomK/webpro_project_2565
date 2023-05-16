31772dfbe5fb1b9328789d5a84d0a387448e34cc
TinnaphoomK <88415686+TinnaphoomK@users.noreply.github.com>
2023-05-16 17:47:37 +0700

    asd

--- client/src/components/CardItem.vue
+++ client/src/components/CardItem.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <div class="z-1 mr-2 ml-5 flex flex-wrap gap-5">
-      <div v-for="(value, index) in room " :key="index" class="card border-round-2xl shadow-5 cursor-pointer"
-        @click="getdetail(value)">
+      <div v-for="(value, index) in filteredRooms" :key="index" class="card border-round-2xl shadow-5 cursor-pointer" @click="getdetail(value)">
         <div class="flex justify-content-start flex-wrap">
           <img class="flex w-17rem h-10rem border-round-top-2xl" :src="value.image" alt="">
         </div>
         <div class="flex bg-white w-17rem h-4rem border-round-bottom-2xl">
-
           <div class="flex justify-content-between p-4 w-full">
             <p class="text-sm text-gray-900 w-7">{{ value.name }}</p>
             <p class="text-sm text-gray-900 w-7">{{ value.description }}</p>
@@ -21,40 +19,43 @@
 
 <script>
 import axios from 'axios';
+
 export default {
   data() {
     return {
-      room: {},
+      rooms: [],
     };
   },
-  props: {floor: String},
-  created: function () {
-    console.log(this.floor)
-    console.log(this.room)
-    this.allroom(this.floor)
+  props: {
+    floor: String,
+  },
+  created() {
+    this.allRooms(this.floor);
   },
   watch: {
     floor: function () {
-      this.allroom(this.floor)
-    }
+      this.allRooms(this.floor);
+    },
+  },
+  computed: {
+    filteredRooms() {
+      return this.rooms.filter(room => room.status === true);
+    },
   },
   methods: {
-    async allroom(floor) {
-      const res = await axios.get('http://localhost:3000/api/room/')
-      console.log(res.data);
+    async allRooms(floor) {
+      const res = await axios.get('http://localhost:3000/api/room/');
 
       if (floor) {
-        console.log(this.floor)
-        this.room = res.data.filter(room => room.floor == this.floor)
+        this.rooms = res.data.filter(room => room.floor == floor);
       } else {
-        this.room = res.data
+        this.rooms = res.data;
       }
     },
     async getdetail(value) {
-      const room = await axios.get(`http://localhost:3000/api/room/${value.id}`)
-      console.log(room)
+      const room = await axios.get(`http://localhost:3000/api/room/${value.id}`);
       this.$router.push(`/detail/${value.id}`);
-    }
-  }
-}
+    },
+  },
+};
 </script>
