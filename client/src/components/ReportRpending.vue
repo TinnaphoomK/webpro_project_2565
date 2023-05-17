39944016601f80cbf61d06eabbe36e4fc907dd29
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-05-18 05:02:56 +0700

    fk u tin

--- client/src/components/ReportRpending.vue
+++ client/src/components/ReportRpending.vue
@@ -63,7 +63,7 @@ export default {
         });
         console.log(res.data);
         this.getAllReports();
-        // window.location.reload();
+        window.location.reload();
       } catch (error) {
         console.log(error);
       }
